6c00c5d94e9c12e39d131b3bd310130f624b4328
Ricrubrom <materomero04@gmail.com>
2022-08-30 23:06:30 -0300

    Terminamos la act 3 de mierda

--- Ejercicios Combinados/Ejercicio3.pas
+++ Ejercicios Combinados/Ejercicio3.pas
@@ -4,12 +4,18 @@ Program ejercicio3;
 Uses 
 sysutils;
 
+
+Const 
+  inf = 5;
+  sup = 10;
+
+
 Type 
   pedido = Record
-    codSeg: integer;
+    codSeg: Integer;
     fechaYhora: string;
-    dni: integer;
-    codArea: integer;
+    dni: longint;
+    codArea: Integer;
     domicilio: string;
     tel: string;
   End;
@@ -24,7 +30,7 @@ Type
   //lista Duplicados
   listaArbol = ^nodo;
   nodo = Record
-    datos: integer;
+    datos: LongInt;
     sig: listaArbol;
   End;
 
@@ -43,6 +49,15 @@ Type
     HI: arbol;
     HD: arbol;
   End;
+
+
+  //Min
+  Min = Record
+    valor: Integer;
+    code: Integer;
+  End;
+
+
 
 
 Procedure agregarLista(Var pri:listaPedidos; p:pedido);
@@ -101,16 +116,16 @@ Var
   cant, cod: integer;
   p: pedido;
 Begin
-  cant := random(10); {genera hasta 100 elementos}
+  cant := random(100); {genera hasta 100 elementos}
   cod := 1;
   While (cant <> 0) Do
     Begin
       p.codSeg := cod;  {codigo de seguimiento}
       p.fechaYhora := cargarFecha();
-      p.dni := random(60000000);  {dni}
-      p.codArea := (random(4000)); {codigo de área}
+      p.dni := random(300);  {dni}
+      p.codArea := (random(15)); {codigo de área}
       p.domicilio := Concat('Domicilio', IntToStr(cod)); {domicilio}
-      p.tel := IntToStr(random(999999)+4000000); {telefono}
+      p.tel := IntToStr(random(99)+400); {telefono}
       agregarLista(l, p);
       cant := cant-1;
       cod := cod+1;
@@ -124,6 +139,7 @@ Begin
 End;
 
 Procedure imprimirLista(l:listaPedidos);
+//como se hablaba? se me cerro el chat jssj
 Begin
   While (l<>Nil) Do
     Begin
@@ -136,6 +152,7 @@ End;
 Procedure CargarArbol (Var a:arbol; l:listaPedidos);
 
 Procedure InsertarLista(Var L: listaArbol; DNI: Integer);
+
 Var nue: listaArbol;
 Begin
   new(nue);
@@ -158,13 +175,14 @@ End;
 
 Begin
   If a = Nil Then
-     CargarNodo(a, n)
+    CargarNodo(a, n)
   Else If a^.datos.code > n.codArea Then insertar (a^.HI, n)
   Else If a^.datos.code < n.codArea Then insertar (a^.HD, n)
-  Else begin
-    InsertarLista(a^.datos.l, n.dni);
-    a^.datos.Tot := a^.datos.Tot + 1;
-  end;
+  Else
+    Begin
+      InsertarLista(a^.datos.l, n.dni);
+      a^.datos.Tot := a^.datos.Tot + 1;
+    End;
 End;
 
 Begin
@@ -175,9 +193,8 @@ Begin
     End;
 End;
 
-//Recorro e imprimo los datos en orden (de menor a mayor)
-Procedure Recorrido (a:arbol);
-Procedure ImprimirLista(l:ListaArbol);
+
+Procedure ImprimirLista2(l:ListaArbol);
 Begin
   While (l<>Nil) Do
     Begin
@@ -186,15 +203,39 @@ Begin
     End;
   writeln('');
 End;
+
+Procedure VerValoresEnRango(a: arbol);
+Begin
+  If (a <> Nil)Then
+    Begin
+      If (a^.datos.code < inf)Then
+        VerValoresEnRango(a^.HD)
+      Else
+        If (a^.datos.code >= inf)And(a^.datos.code <= sup)Then
+          Begin
+            ImprimirLista2(a^.datos.l);
+            VerValoresEnRango(a^.HI);
+            VerValoresEnRango(a^.HD);
+          End;
+      If (a^.datos.code > sup)Then
+        VerValoresEnRango(a^.HI);
+    End;
+End;
+
+
+
+//Recorro e imprimo los datos en orden (de menor a mayor)
+Procedure Recorrido (a:arbol);
 Procedure ImprimirDatos(n: InfoArbol);
 Begin
   writeln('_______________________________________________');
   writeln('Codigo de area: ', n.code);
   writeln('Total de pedidos: ', n.Tot);
   writeln('DNIS: ');
-  ImprimirLista(n.l);
+  ImprimirLista2(n.l);
   writeln('_______________________________________________');
 End;
+
 Begin
   If a<>Nil Then
     Begin
@@ -205,15 +246,19 @@ Begin
 End;
 
 
-Procedure MenorPedido (a:arbol; var Min:integer):integer;
+Function MenorPedido (a:arbol; Var minimo: min): integer;
 Begin
   If a<>Nil Then
     Begin
-      MenorPedido := MenorPedido (a^.HI);
-      if()then
-        MenorPedido := 
-      MenorPedido := MenorPedido (a^.HD);
+      If (a^.datos.Tot< minimo.valor)Then
+        Begin
+          minimo.valor := a^.datos.Tot;
+          Minimo.code := a^.datos.code
+        End;
+      MenorPedido := MenorPedido (a^.HI,minimo);
+      MenorPedido := MenorPedido (a^.HD,minimo);
     End;
+  MenorPedido := minimo.code
 End;
 
 
@@ -221,14 +266,30 @@ End;
 
 
 
-
-
+Procedure ImprimirCodigo (a: arbol; code:Integer);
+Begin
+  If (a=Nil) Then
+    WriteLn ('El dato indicado no se encuentra en el arbol')
+  Else
+    If (a^.datos.code>code) Then
+      ImprimirCodigo (a^.HI, code)
+  Else
+    If (a^.datos.code<code) Then
+      ImprimirCodigo (a^.HD, code)
+  Else
+    Begin
+      writeln('DNIS: ');
+      imprimirLista2(a^.datos.l);
+    End;
+End;
 
 
 
 Var 
   l_inicial: listaPedidos;
   a: arbol;
+  code: integer;
+  minimo: min;
 Begin
   Randomize;
      {Se crea la estructura inicial}
@@ -236,14 +297,20 @@ Begin
   crearLista(l_inicial);
   writeln ('Lista: ');
   imprimirLista(l_inicial);
-
   a := Nil;
   CargarArbol(a, l_inicial); {Inciso A}
   Recorrido(a);
-
-  writeln('El codigo de area con menor cantidad de pedidos es', MenorPedido(a, 0));
-
-
-  writeln('Fin del programa');
-  readln;
+  writeln();
+  minimo.valor := 10000;
+  minimo.code := 1;
+  writeln('El codigo de area con menor cantidad de pedidos es ', MenorPedido(a, minimo)); {inciso B}
+  WriteLn();
+  writeln('Los DNIS con codigo de area entre 5 y 10 son: ');
+  VerValoresEnRango(a); {inciso C}
+  writeln();
+  writeln('Ingrese codigo de area a revisar: ');
+  readln(code);
+  ImprimirCodigo(a, code); {Inciso D}
+  writeln();
+  writeln('Fin de mi vida');
 End.

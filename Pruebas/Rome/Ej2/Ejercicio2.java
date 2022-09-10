1de1d9bab0672014a97e8397b465935db5107eb3
Ricrubrom <materomero04@gmail.com>
2022-09-10 18:09:05 -0300

    Acomode un poco las cosas

---
+++ Pruebas/Rome/Ej2/Ejercicio2.java
@@ -0,0 +1,40 @@
+package Rome.Ej2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            Persona[] v = new Persona[3];
+            String n = new String();
+            int e, i;
+            int dni;
+            for (i = 0; i < 3; i++) {
+                System.out.print("Inserte el Nombre para la persona " + (i + 1) + ": ");
+                n = in.nextLine();
+                System.out.print("Inserte el DNI para la persona " + (i + 1) + ": ");
+                dni = in.nextInt();
+                System.out.print("Inserte la Edad para la persona " + (i + 1) + ": ");
+                e = in.nextInt();
+                v[i] = new Persona(n, dni, e);
+                in.nextLine();
+                System.out.println();
+            }
+            int cant = 0;
+            int minDNI = 999999;
+            String personamin = "";
+            for (i = 0; i < 3; i++) {
+                System.out.println(v[i].toString());
+                System.out.println();
+                if (v[i].mayor65()) {
+                    cant++;
+                }
+                personamin = v[i].min(minDNI);
+                minDNI = v[i].min2(minDNI);
+            }
+            System.out.println("La cantidad de personas mayores a 65 años son: " + cant);
+            System.out.println("La cantidad de personas mayores a 65 años son: " + v[1].jubilados(v));
+            System.out.println(personamin);
+        }
+    }
+}
---
+++ Pruebas/Rome/Ej2/Persona.java
@@ -0,0 +1,80 @@
+package Rome.Ej2;
+
+public class Persona {
+    private String nombre;
+    private int DNI;
+    private int edad;
+
+    public Persona(String unNombre, int unDNI, int unaEdad) {
+        nombre = unNombre;
+        DNI = unDNI;
+        edad = unaEdad;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean mayor65() {
+        if (edad >= 65) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String min(int unDNI) {
+        String aux;
+        if (DNI > unDNI) {
+            aux = "Mi nombre es " + nombre + ", mi DNI es " + DNI + " y tengo " + edad + " años.";
+            return aux;
+        } else {
+            aux = "Mi nombre es " + nombre + ", mi DNI es " + DNI + " y tengo " + edad + " años.";
+            return aux;
+        }
+    }
+
+    public int min2(int unDNI) {
+        if (DNI > unDNI)
+            return unDNI;
+        else
+            return DNI;
+    }
+
+    public int jubilados(Persona[] p) {
+        int cant = 0;
+        for (int i = 0; i < 3; i++) {
+            if (p[i].mayor65()) {
+                cant++;
+            }
+        }
+        return cant;
+    }
+
+    public void setDNI(int unDNI) {
+        DNI = unDNI;
+    }
+
+    public void setEdad(int unaEdad) {
+        edad = unaEdad;
+    }
+
+    public void setNombre(String unNombre) {
+        nombre = unNombre;
+    }
+
+    public String toString() {
+        String aux;
+        aux = "Mi nombre es " + nombre + ", mi DNI es " + DNI + " y tengo " + edad + " años.";
+        return aux;
+    }
+
+}

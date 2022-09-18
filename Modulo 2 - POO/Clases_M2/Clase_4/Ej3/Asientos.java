d84e538d53e86c5581275504f67f6c8894b13162
Ricrubrom <materomero04@gmail.com>
2022-09-18 19:41:41 -0300

    Ejercicio 3

---
+++ Modulo 2 - POO/Clases_M2/Clase_4/Ej3/Asientos.java
@@ -0,0 +1,45 @@
+package Ej3;
+
+public class Asientos {
+    private boolean[] asientos;
+    private int ocupados;
+    private int tot;
+
+    public Asientos() {
+        this.asientos = new boolean[20];
+        this.tot = 20;
+        for (int i = 0; i < 20; i++)
+            this.asientos[i] = false;
+    }
+
+    public boolean getAsientos(int i) {
+        return asientos[i];
+    }
+
+    public int getOcupados() {
+        return ocupados;
+    }
+
+    public int getTot() {
+        return tot;
+    }
+
+    public void ocuparAsiento(int asiento) {
+        this.asientos[asiento] = true;
+        ocupados++;
+    }
+
+    public void liberarAsiento(int asiento) {
+        this.asientos[asiento] = false;
+        ocupados--;
+    }
+
+    public int devolverLibre() {
+        int i = 0;
+        while (asientos[i] != false)
+            i++;
+        return i;
+
+    }
+
+}
